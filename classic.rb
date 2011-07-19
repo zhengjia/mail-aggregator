1efcb1df0ffb5e12558cb6272edeeb50b4523867
zhengjia <jiazheng@live.com>
2011-07-18 21:25:59 -0500

    add files

---
+++ classic.rb
@@ -0,0 +1,23 @@
+require_relative 'lib/load_path'
+require 'post'
+require 'db_connection'
+require 'sinatra'
+require 'padrino-helpers'
+require 'rack-flash'
+
+enable :sessions
+
+Sinatra.register Padrino::Helpers
+use Rack::Flash
+
+get "/" do
+  @posts = Post.visible
+  erb :index
+end
+
+post "/mark_hide/:id" do
+  @post = Post.find(params[:id])
+  @post.update_attributes(:visible => false)
+  flash[:notice] = "Successfully hide post #{@post.subject}. id #{@post.id}"
+  redirect "/"
+end
---
+++ config.ru
@@ -0,0 +1,2 @@
+require File.expand_path('../classic', __FILE__)
+run Sinatra::Application
