faa614dd26b3c62f0d6b8a273987f9e56f1a94e7
zhengjia <jiazheng@live.com>
2011-08-02 23:14:35 -0500

    shady pagination

--- classic.rb
+++ classic.rb
@@ -6,12 +6,19 @@ require 'padrino-helpers'
 require 'rack-flash'
 
 enable :sessions
+set :per_page, 20
 
 Sinatra.register Padrino::Helpers
 use Rack::Flash
 
 get "/" do
-  @posts = Post.visible.order_by([:date, :desc])
+  page = params[:page].try(:to_i) || 1
+  @posts = Post.visible.order_by([:date, :desc]).skip( (page-1)*20 ).limit(settings.per_page)
+  @posts.instance_eval do
+    def num_of_page
+      (Post.visible.length / settings.per_page.to_f).ceil.to_i
+    end
+  end
   erb :index
 end
 
@@ -19,5 +26,5 @@ post "/mark_hide/:id" do
   @post = Post.find(params[:id])
   @post.update_attributes(:visible => false)
   flash[:notice] = "Successfully hide post #{@post.subject}. id #{@post.id}"
-  redirect "/"
+  redirect "/?page=#{params[:page]}"
 end
